b91276bf28510c95070ecb47f3632afdf89012d7
Andres Avila <46364118+Andres-Avila1601@users.noreply.github.com>
2019-03-27 19:23:52 -0500

    NEW WORK!!

---
+++ js/adsbygoogle.js
@@ -0,0 +1,22 @@
+var loadingAds=true;
+/*
+     FILE ARCHIVED ON 18:52:01 Aug 09, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 23:10:52 Mar 27, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 69.679 (3)
+  esindex: 0.02
+  captures_list: 94.007
+  CDXLines.iter: 13.127 (3)
+  PetaboxLoader3.datanode: 414.994 (5)
+  exclusion.robots: 0.298
+  exclusion.robots.policy: 0.276
+  RedisCDXSource: 6.59
+  PetaboxLoader3.resolve: 41.903
+  load_resource: 415.217
+*/
---
+++ js/animeyt.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+        window.mobileAndTabletcheck = function(){
+            var check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent || navigator.vendor || window.opera);
+            return check;
+        };
+	
+	
+if (window.mobileAndTabletcheck() == true){
+var el = document.getElementById("some-element");
+el.setAttribute("style", "position: relative;z-index: 1000;display:block;");
+var elx = document.getElementById("principal");
+elx.setAttribute("style", "display:none;");
+document.getElementById('search-box').className='main-search-boxx';	
+var elxx = document.getElementById("twit");
+elxx.setAttribute("style", "display:block;");
+var elemento=document.getElementById('YTE');
+elemento.parentNode.removeChild(elemento);
+}else{   
+var elx = document.getElementById("principal");
+elx.setAttribute("style", "display:block;");	
+var elemento=document.getElementById('YTM');
+elemento.parentNode.removeChild(elemento);
+document.getElementById("YTE").innerHTML = '<div id="M131797ScriptRootC213298"><div id="M131797PreloadC213298">					Loading...    </div></div>';
+var terras = document.getElementById("div-gpt-ad-2");
+terras.setAttribute("style", "display:none;"); 
+}
+
+(function(){
+var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById';
+var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M131797ScriptRootC213298")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
+catch(e){var iw=d;var c=d[gi]("M131797ScriptRootC213298");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=213298;c[ac](dv);
+var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src="//web.archive.org/web/20180809185201/https://jsc.mgid.com/a/n/animeyt.tv.213298.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
+	
+});
+/*
+     FILE ARCHIVED ON 18:52:01 Aug 09, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 23:10:00 Mar 27, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 75.423 (3)
+  esindex: 0.007
+  captures_list: 92.525
+  CDXLines.iter: 8.943 (3)
+  PetaboxLoader3.datanode: 80.438 (5)
+  exclusion.robots: 0.132
+  exclusion.robots.policy: 0.12
+  RedisCDXSource: 5.587
+  PetaboxLoader3.resolve: 72.909 (3)
+  load_resource: 98.718
+*/
